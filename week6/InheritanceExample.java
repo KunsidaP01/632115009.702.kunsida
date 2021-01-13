47058e2ad79bd98d8eb33be22fb760522829b69f
KunsidaP01 <kunsida_p@elearning.cmu.ac.th>
2021-01-13 19:02:01 +0700

    enter

---
+++ week6/InheritanceExample.java
@@ -0,0 +1,30 @@
+package week6;
+
+public class InheritanceExample {
+    public static void main(String[] args){
+        Artist art = new Artist("Ball", 20,"Male");
+        art.genre="hip hop";
+        art.introduce();
+        art.playMusic();
+        
+        Job1 job1=new Job1();
+        job1.differentJob="Doctor";
+        job1.DisplayJob();
+        
+        Job2 job2=new Job2();
+        job2.differentJob="Engineer";
+        job2.DisplayJob();
+        
+        Job3 job3=new Job3();
+        job3.differentJob="Programmer";
+        job3.DisplayJob();
+
+        Job4 job4=new Job4();
+        job4.differentJob="Singer";
+        job4.DisplayJob();
+
+        Job5 job5=new Job5();
+        job5.differentJob="Police";
+        job5.DisplayJob();
+    }
+}
