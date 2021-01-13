d3039d9dfc83fa147d93d21b7b36dc0fc9128172
KunsidaP01 <kunsida_p@elearning.cmu.ac.th>
2021-01-13 11:31:49 +0700

    edit

--- test/Hello.java
+++ test/Hello.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-package test;
-public class Hello {
-    public static void  main(String[] args){
-        System.out.print("Hello yeyeye");
-    }
-}
-=======
 package test;
 public class Hello {
     public static void  main(String[] args){
         System.out.print("Hello yeyeyeee");
     }
 }
->>>>>>> 7e14495dd3af715cbcd539a4dbfe30943cd6c072
