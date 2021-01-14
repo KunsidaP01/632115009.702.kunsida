0cca1218ac9f5b8d0caa243af34927cb07f1b357
KunsidaP01 <77325047+KunsidaP01@users.noreply.github.com>
2021-01-14 11:00:32 +0700

    Update Main.java

--- week5/Main.java
+++ week5/Main.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-      
+        
+        //hello
         //632115009 Kunsida Phitakam
         
         Scanner input=new Scanner(System.in);
