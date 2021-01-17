d4fb739d5b96dee4dfc94108655e082bebebef55
KunsidaP01 <kunsida_p@elearning.cmu.ac.th>
2021-01-18 06:18:41 +0700

    edit

--- week7/Main.java
+++ week7/Main.java
@@ -4,9 +4,20 @@ public class Main {
     public static void main(String[] args){
         Dog dog=new Dog("Lucky","Pooddle","brown",2);
         dog.kind="dog";
+        dog.size="Small";
         dog.showPetData();
         dog.Run("to the jungle");
-        
+        dog.Bark("Box Box!!");
+        dog.isShortHair("Short hair");
+
+        System.out.println("\n");
+        Fish fish=new Fish("Nemo","clownfish","orange and white",3);
+        fish.kind="fish";
+        fish.showPetData();
+        fish.swimSpeed=10;
+        fish.size="Small";
+        fish. SwimFast();
+        fish.isSwimToTheSea("Swim to the sea");
     }
 
     
--- week7/Pet.java
+++ week7/Pet.java
@@ -16,7 +16,7 @@ public class Pet{
 }
 class Dog extends Pet{
     String size;
-    String hair;
+    String checkHair;
     public Dog(String name,String species,String color,int age){
         this.name=name;
         this.species=species;
@@ -26,4 +26,33 @@ class Dog extends Pet{
     public void Run(String run){
         System.out.println("My "+kind+" is run "+run);
     } 
+    public void Bark(String bark){
+        System.out.println(size+" bark "+bark);
+    }
+    public void isShortHair(String hair){
+        if(hair.equals("Short hair")){
+            System.out.println("Short hair: true");
+        }
+        else System.out.println("Short hair: false");
+          
+    }  
+}
+class Fish extends Pet{
+    String size,saltOrFreshWater;
+    int swimSpeed;
+    public Fish(String name,String species,String color,int age){
+        this.name=name;
+        this.species=species;
+        this.color=color;
+        this.age=age;
+    }
+    public void isSwimToTheSea(String swimWhere){
+        if(swimWhere.equals("Swim to the sea")){
+            System.out.println("My "+kind+" is swim to the sea");
+        }
+        else System.out.println("My "+kind+"is not wim to the sea");
+    }
+    public void SwimFast(){
+        System.out.println(size+" Swim Speed: "+swimSpeed+" cm/s");
+    }
 }
