0e95cabea37117494291e45c07cf9b9f8ed474c0
KunsidaP01 <kunsida_p@elearning.cmu.ac.th>
2021-01-22 00:37:25 +0700

    3

--- week7/Bicycle.java
+++ week7/Bicycle.java
@@ -1,29 +1,52 @@
 package week7;
 public class Bicycle {
     int currentSpeed;
+    int increase=5;
     public Bicycle(){
     }
 }
 //สร้างclass extends break speed up
-class TheSpeed extends Bicycle{
-    public TheSpeed(int speed){
+class NormalBicycle extends Bicycle{
+    public NormalBicycle(int speed){
         currentSpeed=speed;
     }
     public void apply(String apply){
 
         if(apply.equals("speed")){
-        this.currentSpeed+=5;
+        this.currentSpeed+=increase;
             if(currentSpeed>99){
-                currentSpeed-=5;
+                currentSpeed-=increase;
                 System.out.println("The current speed is too much!!");
             }    
             System.out.println("Current speed : "+currentSpeed);
         }
         else if(apply.equals("break")){
-            this.currentSpeed-=5;
+            this.currentSpeed-=increase;
             System.out.println("Current speed : "+currentSpeed);   
             }
         
     }    
+}
+class MountainBicycle extends Bicycle{
+    int gear=2;
+    public MountainBicycle(int speed){
+        this.currentSpeed=speed;
+    }
+    public void apply(String apply){
+
+        if(apply.equals("speed")){
+        this.currentSpeed+=(increase*gear);
+            if(currentSpeed>99){
+                currentSpeed-=increase;
+                System.out.println("The current speed is too much!!");
+            }    
+            System.out.println("Current speed : "+currentSpeed);
+        }
+        else if(apply.equals("break")){
+            this.currentSpeed-=increase;
+            System.out.println("Current speed : "+currentSpeed);   
+            }
+        
+    }   
 }
 
--- week7/MainBicycle.java
+++ week7/MainBicycle.java
@@ -3,18 +3,35 @@ import java.util.Scanner;
 public class MainBicycle {
     public static void main(String[] args){
         Scanner input =new Scanner(System.in);
+        
         String apply;
 
-        TheSpeed commonBicycle = new TheSpeed(90);
-        System.out.println("Enter 'speed' to speed up or 'break' to break ,If you want to stop enter quit");
-    
+        NormalBicycle commonBicycle = new NormalBicycle(10);
+        MountainBicycle mountainBicycle = new MountainBicycle(10);
+
+        System.out.println("\n");
+        System.out.println("Please choose type of bicycle, Bicycle or Mountain");
+        String type=input.next();
+
+        if(type.equals("Bicycle")){
+            System.out.println("Enter 'speed' to speed up or 'break' to break ,If you want to stop enter quit");
+            System.out.println("Now ,the current speed is "+commonBicycle.currentSpeed);
        do{
            System.out.print("Please enter: ");
                apply=input.next();
                commonBicycle.apply(apply);
        }while(!apply.equals("quit"));
-        
-        
+        }
+        else if(type.equals("Mountain")){
+            System.out.println("Enter 'speed' to speed up or 'break' to break ,If you want to stop enter quit");
+            System.out.println("Now ,the current speed is "+commonBicycle.currentSpeed);
+       do{
+           System.out.print("Please enter: ");
+               apply=input.next();
+               mountainBicycle.apply(apply);
+       }while(!apply.equals("quit"));
+        }   
 input.close();
     }
 }
+//เลือกโหมด หน้านี้ใช้if 
