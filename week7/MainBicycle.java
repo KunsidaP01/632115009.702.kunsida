a524548e27693dcd9b75275fc7831f34f3ce046a
KunsidaP01 <77325047+KunsidaP01@users.noreply.github.com>
2021-01-22 17:09:29 +0700

    5

--- week7/MainBicycle.java
+++ week7/MainBicycle.java
@@ -2,6 +2,8 @@ package week7;
 import java.util.Scanner;
 public class MainBicycle {
     public static void main(String[] args){
+        
+        //632115009 Kunsida Phitakham
         Scanner input =new Scanner(System.in);
         
         String apply;
